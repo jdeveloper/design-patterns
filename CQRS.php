3784efeb9b13b501cd64caeecc899f04db7e7588
jota <jdeveloper.inxenio@gmail.com>
2019-06-25 20:39:23 +0200

    Added cqrs example

---
+++ CQRS.php
@@ -0,0 +1,73 @@
+<?php
+interface Event
+{
+
+}
+
+class Monedero
+{
+    protected id;
+
+    protected $saldo = 0;
+
+    protected $events = [];
+
+    public function compraOpocoins($opocoins)
+    {
+        $this->recordAndApplyEvent(new OpocoinsComprados($opocoins, $this));
+    }
+
+    public function gastaOpocoins($opocoins)
+    {
+        if($this->saldo >= $opocoins){
+            $this->recordAndApplyEvent(new OpocoinsGastados($opocoins, $this));
+        }
+    }
+
+    public function getEvents()
+    {
+        return $this->events;
+    }
+
+    public function applyEvent(Event $event)
+    {
+        if($event instanceof OpocoinsComprados){
+            $this->saldo += $event->getOpocoins();
+        }else if($event instanceof OpocoinsGastados){
+            $this->saldo -= $event->getOpocoins();
+        }
+    }
+
+    private function recordAndApplyEvent(Event $event)
+    {
+        $this->events[] = $event;
+        $this->applyEvent($event);
+    }
+}
+
+class ComprarOpocoins
+{
+    protected $eventBus;
+
+    protected $monedero;
+
+    protected $opocoins;
+
+    public function execute()
+    {
+        $this->monedero->compraOpocoins($this->opocoins);
+        $this->eventBus->publishEvents($modero->getEvents());
+    }
+}
+
+class OpocoinsCompratosToDBHandler
+{
+    public function handle(OpocoinsComprados $event)
+    {
+        $sql = "UPDATE monedero 
+             SET saldo = saldo + ".$event->getOpocoins()." 
+             WHERE id = ".$event->getMonedero()->getid();
+
+        $this->executeSql($sql);
+    }
+}
